11fd2e1a0f2d1f207a52fd909d66c1f609af76c3
Jamison Griffith <jamison@zingstudios.com>
2022-12-27 14:35:45 -0700

    Add stylings

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,71 @@
 import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import TestComponent from "./TestComponent";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const increaseCount = () => {
-    setCount(prevCount => prevCount + 1);
-  };
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <TestComponent name={"Jamo"}></TestComponent>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>
-          Check out the count: <span style={{color: 'cyan'}}>{ count }</span>
-        </p>
-        <button onClick={increaseCount}>Click</button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo-app-container">
+        <div className="todo-app">
+          <div className="header">
+            <img className={"logo"} src={logo}/>
+            <h1>React ToDo</h1>
+          </div>
+          <form action="#">
+            <input
+              type="text"
+              className="todo-input"
+              placeholder="What do you need to do?"
+            />
+          </form>
+
+          <ul className="todo-list">
+            {/*{this.state.todos.map((todo, index) => (
+              <li className="todo-item-container">
+                <div className="todo-item">
+                  <input type="checkbox" />
+                  <span className="todo-item-label">{todo.title}</span>
+                   <input type="text" className="todo-item-input" value="Finish React Series" />
+                </div>
+                <button className="x-button">
+                  <svg
+                    className="x-button-icon"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                </button>
+              </li>
+            ))}*/}
+          </ul>
+
+          <div className="check-all-container">
+            <div>
+              <div className="button">Check All</div>
+            </div>
+
+            <span>3 items remaining</span>
+          </div>
+
+          <div className="other-buttons-container">
+            <div>
+              <button className="button filter-button filter-button-active">
+                All
+              </button>
+              <button className="button filter-button">Active</button>
+              <button className="button filter-button">Completed</button>
+            </div>
+            <div>
+              <button className="button">Clear completed</button>
+            </div>
+          </div>
+        </div>
     </div>
   );
 }
