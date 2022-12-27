5f16d4450723543bc135727c595c828cf7aadec3
Jamison Griffith <jamison@zingstudios.com>
2022-12-27 15:33:47 -0700

    Adds reactivity to adding ToDos

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,74 @@
-import React, {useState} from 'react';
+import React, {ChangeEvent, FormEvent, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+export interface ToDo {
+  id: number,
+  title: string,
+  isComplete: boolean
+}
+
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: 'Modify styles',
+      isComplete: true
+    },
+    {
+      id: 2,
+      title: 'Add state',
+      isComplete: false
+    },
+    {
+      id: 3,
+      title: 'Practice Typescript',
+      isComplete: false
+    },
+  ] as ToDo[]);
+
+  const [todoInput, setTodoInput] = useState('');
+
+  const addTodo = (event: FormEvent) => {
+    event.preventDefault(); // don't execute form action
+
+    let lastTodo = todos.at(-1);
+
+    let newTodo: ToDo = {
+      id: lastTodo?.id ? lastTodo.id + 1 : 0,
+      title: todoInput,
+      isComplete: false
+    };
+    setTodos([...todos, newTodo]);
+    setTodoInput('');
+  };
+
+  const handleInput = (event: ChangeEvent) => {
+    // Interesting... need to cast as type here because
+    // any given HTMLElement isn't guaranteed to have a value property
+    setTodoInput((event.target as HTMLInputElement).value);
+  };
+
   return (
     <div className="todo-app-container">
         <div className="todo-app">
           <div className="header">
             <img className={"logo"} src={logo}/>
-            <h1>React ToDo</h1>
+            <h1>React ToDo Demo</h1>
           </div>
-          <form action="#">
+          <form action="#" onSubmit={addTodo}>
             <input
               type="text"
+              value={todoInput}
+              onChange={handleInput}
               className="todo-input"
-              placeholder="What do you need to do?"
+              placeholder="What's next?"
             />
           </form>
 
           <ul className="todo-list">
-            {/*{this.state.todos.map((todo, index) => (
-              <li className="todo-item-container">
+            {todos.map((todo, index) => (
+              <li key={todo.id} className="todo-item-container">
                 <div className="todo-item">
                   <input type="checkbox" />
                   <span className="todo-item-label">{todo.title}</span>
@@ -42,7 +90,7 @@ function App() {
                   </svg>
                 </button>
               </li>
-            ))}*/}
+            ))}
           </ul>
 
           <div className="check-all-container">
